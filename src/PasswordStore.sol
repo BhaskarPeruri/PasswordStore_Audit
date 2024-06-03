039f3eec83bd75dc4ea07d058e523e11243ba46e
Satya Bhaskar <satyabhaskar@Satyas-MacBook-Air.local>
2024-06-03 16:03:09 +0530

    first commit

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -11,6 +11,7 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    //@audit the s_password variable is not actually private!. This is not a safe place to secure your password!
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +24,7 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    //@audit -high any user can set the password.  ---missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -32,6 +34,7 @@ contract PasswordStore {
      * @notice This allows only the owner to retrieve the password.
      * @param newPassword The new password to set.
      */
+    //@audit there is no param newPassword to set.
     function getPassword() external view returns (string memory) {
         if (msg.sender != s_owner) {
             revert PasswordStore__NotOwner();
